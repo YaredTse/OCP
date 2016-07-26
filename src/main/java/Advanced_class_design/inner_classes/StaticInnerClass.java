5ad1ed77eb95f1f643453ee7bda1da1f17e502f8
Yared Tseghu <jaredtse@hotmail.com>
2016-07-26 17:28:20 +0100

    Additional test cases

--- src/main/java/Advanced_class_design/inner_classes/StaticInnerClass.java
+++ src/main/java/Advanced_class_design/inner_classes/StaticInnerClass.java
@@ -2,6 +2,18 @@ package Advanced_class_design.inner_classes;
 
 public class StaticInnerClass {
 
+    static class InnerStatic{
+        public String retutnName( String str ){
+            System.out.println(" = = = = = = = = ");
+            return str;
+        }
+
+        static public String staticMethod( String str ){
+            return str.toUpperCase();
+        }
+
+    }
+
     String name;
 
     public int returnLength(String s ){
